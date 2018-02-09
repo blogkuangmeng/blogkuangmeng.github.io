46e99cd881a97dc6e50be3cdfb0974ddad1a3366
kuangmeng <kuangmeng@msn.com>
2018-02-09 18:55:13 +0800

    Site updated: 2018-02-09 18:55:09

--- js/dialogFx.js
+++ js/dialogFx.js
@@ -8,93 +8,105 @@
  * Copyright 2014, Codrops
  * http://www.codrops.com
  */
-;( function( window ) {
-	
-	'use strict';
+;
+(function(window) {
 
-	var support = { animations : Modernizr.cssanimations },
-		animEndEventNames = { 'WebkitAnimation' : 'webkitAnimationEnd', 'OAnimation' : 'oAnimationEnd', 'msAnimation' : 'MSAnimationEnd', 'animation' : 'animationend' },
-		animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ],
-		onEndAnimation = function( el, callback ) {
-			var onEndCallbackFn = function( ev ) {
-				if( support.animations ) {
-					if( ev.target != this ) return;
-					this.removeEventListener( animEndEventName, onEndCallbackFn );
-				}
-				if( callback && typeof callback === 'function' ) { callback.call(); }
-			};
-			if( support.animations ) {
-				el.addEventListener( animEndEventName, onEndCallbackFn );
-			}
-			else {
-				onEndCallbackFn();
-			}
-		};
+  'use strict';
 
-	function extend( a, b ) {
-		for( var key in b ) { 
-			if( b.hasOwnProperty( key ) ) {
-				a[key] = b[key];
-			}
-		}
-		return a;
-	}
+  var support = {
+      animations: Modernizr.cssanimations
+    },
+    animEndEventNames = {
+      'WebkitAnimation': 'webkitAnimationEnd',
+      'OAnimation': 'oAnimationEnd',
+      'msAnimation': 'MSAnimationEnd',
+      'animation': 'animationend'
+    },
+    animEndEventName = animEndEventNames[Modernizr.prefixed('animation')],
+    onEndAnimation = function(el, callback) {
+      var onEndCallbackFn = function(ev) {
+        if (support.animations) {
+          if (ev.target != this) return;
+          this.removeEventListener(animEndEventName, onEndCallbackFn);
+        }
+        if (callback && typeof callback === 'function') {
+          callback.call();
+        }
+      };
+      if (support.animations) {
+        el.addEventListener(animEndEventName, onEndCallbackFn);
+      } else {
+        onEndCallbackFn();
+      }
+    };
 
-	function DialogFx( el, options ) {
-		this.el = el;
-		this.options = extend( {}, this.options );
-		extend( this.options, options );
-		this.ctrlClose = this.el.querySelector( '[data-dialog-close]' );
-		this.isOpen = false;
-		this._initEvents();
-	}
+  function extend(a, b) {
+    for (var key in b) {
+      if (b.hasOwnProperty(key)) {
+        a[key] = b[key];
+      }
+    }
+    return a;
+  }
 
-	DialogFx.prototype.options = {
-		// callbacks
-		onOpenDialog : function() { return false; },
-		onCloseDialog : function() { return false; }
-	}
+  function DialogFx(el, options) {
+    this.el = el;
+    this.options = extend({}, this.options);
+    extend(this.options, options);
+    this.ctrlClose = this.el.querySelector('[data-dialog-close]');
+    this.isOpen = false;
+    this._initEvents();
+  }
 
-	DialogFx.prototype._initEvents = function() {
-		var self = this;
+  DialogFx.prototype.options = {
+    // callbacks
+    onOpenDialog: function() {
+      return false;
+    },
+    onCloseDialog: function() {
+      return false;
+    }
+  }
 
-		// close action
-		this.ctrlClose.addEventListener( 'click', this.toggle.bind(this) );
+  DialogFx.prototype._initEvents = function() {
+    var self = this;
 
-		// esc key closes dialog
-		document.addEventListener( 'keydown', function( ev ) {
-			var keyCode = ev.keyCode || ev.which;
-			if( keyCode === 27 && self.isOpen ) {
-				self.toggle();
-			}
-		} );
+    // close action
+    this.ctrlClose.addEventListener('click', this.toggle.bind(this));
 
-		this.el.querySelector( '.dialog__overlay' ).addEventListener( 'click', this.toggle.bind(this) );
-	}
+    // esc key closes dialog
+    document.addEventListener('keydown', function(ev) {
+      var keyCode = ev.keyCode || ev.which;
+      if (keyCode === 27 && self.isOpen) {
+        self.toggle();
+      }
+    });
 
-	DialogFx.prototype.toggle = function() {
-		var self = this;
-		if( this.isOpen ) {
-			classie.remove( this.el, 'dialog--open' );
-			classie.add( self.el, 'dialog--close' );
-			
-			onEndAnimation( this.el.querySelector( '.dialog__content' ), function() {
-				classie.remove( self.el, 'dialog--close' );
-			} );
+    this.el.querySelector('.dialog__overlay').addEventListener('click', this.toggle.bind(this));
+  }
 
-			// callback on close
-			this.options.onCloseDialog( this );
-		}
-		else {
-			classie.add( this.el, 'dialog--open' );
+  DialogFx.prototype.toggle = function() {
+    var self = this;
+    if (this.isOpen) {
+      classie.remove(this.el, 'dialog--open');
+      classie.add(self.el, 'dialog--close');
 
-			// callback on open
-			this.options.onOpenDialog( this );
-		}
-		this.isOpen = !this.isOpen;
-	};
+      onEndAnimation(this.el.querySelector('.dialog__content'), function() {
+        classie.remove(self.el, 'dialog--close');
+      });
 
-	// add to global namespace
-	window.DialogFx = DialogFx;
+      // callback on close
+      this.options.onCloseDialog(this);
+    } else {
+      classie.add(this.el, 'dialog--open');
 
-})( window );
+      // callback on open
+      this.options.onOpenDialog(this);
+    }
+    this.isOpen = !this.isOpen;
+  };
+
+  // add to global namespace
+  window.DialogFx = DialogFx;
+
+})(window);
--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,50 @@
-(function($){
+(function($) {
   // Search
   var $searchWrap = $('#search-form-wrap'),
     isSearchAnim = false,
     searchAnimDuration = 200;
 
-  var startSearchAnim = function(){
+  var startSearchAnim = function() {
     isSearchAnim = true;
   };
 
-  var stopSearchAnim = function(callback){
-    setTimeout(function(){
+  var stopSearchAnim = function(callback) {
+    setTimeout(function() {
       isSearchAnim = false;
       callback && callback();
     }, searchAnimDuration);
   };
 
   var s = [
-        '<div style="display: none;">',
-          '<script src="https://s11.cnzz.com/z_stat.php?id=1260716016&web_id=1260716016" language="JavaScript"></script>',
-        '</div>'
-      ].join('');
+    '<div style="display: none;">',
+    '<script src="https://s11.cnzz.com/z_stat.php?id=1260716016&web_id=1260716016" language="JavaScript"></script>',
+    '</div>'
+  ].join('');
 
   var di = $(s);
 
   $('#container').append(di);
 
-  $('#nav-search-btn').on('click', function(){
+  $('#nav-search-btn').on('click', function() {
     if (isSearchAnim) return;
 
     startSearchAnim();
     $searchWrap.addClass('on');
-    stopSearchAnim(function(){
+    stopSearchAnim(function() {
       $('.search-form-input').focus();
     });
   });
 
-  $('.search-form-input').on('blur', function(){
+  $('.search-form-input').on('blur', function() {
     startSearchAnim();
     $searchWrap.removeClass('on');
     stopSearchAnim();
   });
 
   // Share
-  $('body').on('click', function(){
+  $('body').on('click', function() {
     $('.article-share-box.on').removeClass('on');
-  }).on('click', '.article-share-link', function(e){
+  }).on('click', '.article-share-link', function(e) {
     e.stopPropagation();
 
     var $this = $(this),
@@ -53,23 +53,23 @@
       id = 'article-share-box-' + $this.attr('data-id'),
       offset = $this.offset();
 
-    if ($('#' + id).length){
+    if ($('#' + id).length) {
       var box = $('#' + id);
 
-      if (box.hasClass('on')){
+      if (box.hasClass('on')) {
         box.removeClass('on');
         return;
       }
     } else {
       var html = [
         '<div id="' + id + '" class="article-share-box">',
-          '<input class="article-share-input" value="' + url + '">',
-          '<div class="article-share-links">',
-            '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
-            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
-            '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
-            '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
-          '</div>',
+        '<input class="article-share-input" value="' + url + '">',
+        '<div class="article-share-links">',
+        '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
+        '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
+        '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
+        '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="article-share-google" target="_blank" title="Google+"></a>',
+        '</div>',
         '</div>'
       ].join('');
 
@@ -84,11 +84,11 @@
       top: offset.top + 25,
       left: offset.left
     }).addClass('on');
-  }).on('click', '.article-share-box', function(e){
+  }).on('click', '.article-share-box', function(e) {
     e.stopPropagation();
-  }).on('click', '.article-share-box-input', function(){
+  }).on('click', '.article-share-box-input', function() {
     $(this).select();
-  }).on('click', '.article-share-box-link', function(e){
+  }).on('click', '.article-share-box-link', function(e) {
     e.preventDefault();
     e.stopPropagation();
 
@@ -96,8 +96,8 @@
   });
 
   // Caption
-  $('.article-entry').each(function(i){
-    $(this).find('img').each(function(){
+  $('.article-entry').each(function(i) {
+    $(this).find('img').each(function() {
       if ($(this).parent().hasClass('fancybox')) return;
 
       var alt = this.alt;
@@ -107,12 +107,12 @@
       $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
     });
 
-    $(this).find('.fancybox').each(function(){
+    $(this).find('.fancybox').each(function() {
       $(this).attr('rel', 'article' + i);
     });
   });
 
-  if ($.fancybox){
+  if ($.fancybox) {
     $('.fancybox').fancybox();
   }
 
@@ -121,17 +121,17 @@
     isMobileNavAnim = false,
     mobileNavAnimDuration = 200;
 
-  var startMobileNavAnim = function(){
+  var startMobileNavAnim = function() {
     isMobileNavAnim = true;
   };
 
-  var stopMobileNavAnim = function(){
-    setTimeout(function(){
+  var stopMobileNavAnim = function() {
+    setTimeout(function() {
       isMobileNavAnim = false;
     }, mobileNavAnimDuration);
   }
 
-  $('#main-nav-toggle').on('click', function(){
+  $('#main-nav-toggle').on('click', function() {
     if (isMobileNavAnim) return;
 
     startMobileNavAnim();
@@ -139,7 +139,7 @@
     stopMobileNavAnim();
   });
 
-  $('#wrap').on('click', function(){
+  $('#wrap').on('click', function() {
     if (isMobileNavAnim || !$container.hasClass('mobile-nav-on')) return;
 
     $container.removeClass('mobile-nav-on');
